881afa4163b25d2150c58d346df57c1f941c2bd6
Noritaka Horio <holy.shared.design@gmail.com>
2010-01-20 01:45:31 +0900

    demo changes.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Demos/js/vertical.js
+++ Demos/js/vertical.js
@@ -1,38 +1,65 @@
-window.addEvent("domready", function() {
+var Vertical = {
 
-	$("container").setStyle("height", document.body.getSize().y);
-/*
-	new Tips.Pointy($("exhibition").getElements("li a img"), {
-		pointyTipOptions: {
-		    point: 6,
-		    width: 200,
-		    pointyOptions: {
-		        closeButton: true
-		    }
-		}
-	});
-*/
-	var activeTip = null;
-	var exhibition = new Exhibition.Vertical($("exhibition"), $("exhibition").getElements("li"), {
-		"onChange": function(index,element) {
-			var img = element.getElement("a img");
-			var props = $(img).getProperties("title", "alt");
-			if (activeTip) { activeTip.hide() };
-			activeTip = new StickyWin.PointyTip(props.title, props.alt, {point: 'left', relativeTo: element});
-		},
-		"onNext": function(index,element) {
-		},
-		"onPrev": function(index,element) {
-		}
-	});
-	
-	$("prev").addEvent("click", function() {
-		exhibition.prev();
-	});
+	tags: ["hat", "cap", "hats", "caps", "faves", 'favorite'],
 
-	$("next").addEvent("click", function() {
-		exhibition.next();
-	});
-	
-	
-});
+	initialize: function() {
+		this.container = $("exhibition");
+		this.container.setStyle("height", window.innerHeight);
+
+		this.loadImages = new Array();
+		this.flickr = new Request.JSONP({
+			'url': "http://api.flickr.com/services/rest/",
+			'data': {
+				'api_key': 'c5d9b8efc69355b80e96d53ccc2fce98',
+				'method': 'flickr.photos.search',
+				'sort': 'date-posted-desc',
+				'tags': this.tags.join(","),
+				'tag_mode': 'OR',
+				'per_page': 20,
+				'format': 'json'
+			},
+			'callbackKey': 'jsoncallback',
+			'onSuccess': this.onSuccess.bind(this)
+		});
+		this.flickr.send();
+	},
+
+	onSuccess: function(response) {
+		this.response = response;
+		if (response.stat == 'ok') {
+			var photos = response.photos.photo;
+			var preloadImages = new Array();
+			photos.each(function(photo) {
+				var src = "http://farm" + photo.farm + ".static.flickr.com/";
+				src += photo.server + "/" + photo.id + "_" + photo.secret + "_t.jpg";
+
+				preloadImages.push(src);
+			});
+
+			this.loadImages = new Asset.images(preloadImages, {
+				onProgress: this.onLoad.bind(this),
+				onComplete: this.onPreload.bind(this)
+			});
+		}
+	},
+
+	onLoad: function(counter,index) {
+		var photos = this.response.photos.photo;
+		var a  = new Element("a", {"href": "#", "title": photos[index].title});
+		var li = new Element("li");
+		this.loadImages[index].inject(a);
+		a.inject(li);
+		li.inject(this.container);
+	},
+
+	onPreload: function() {
+		this.createExhibition();
+		new Tips.Pointy(this.container.getElements("li a"), {pointyTipOptions: { point: 12, width: 150 }});
+	},
+
+	createExhibition: function() {
+		var exhibition = new Exhibition.Vertical(this.container, this.container.getElements("li"));
+	}
+}
+
+window.addEvent("domready", Vertical.initialize.bind(Vertical));
