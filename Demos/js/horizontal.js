7bb69a4cfdcaab04a3c57f0c0dd19cc3377b2465
holy <holy.shared.design@gmail.com>
2010-02-01 13:10:43 +0900

    add loading logic.
    Signed-off-by: holy <holy.shared.design@gmail.com>

--- Demos/js/horizontal.js
+++ Demos/js/horizontal.js
@@ -12,6 +12,8 @@ var Horizontal = {
 	//Flickr API URL
 	flickAPIURL: "http://api.flickr.com/services/rest/",
 
+	perPage: 20,
+	
 	//image caches
 	cache: [],
 
@@ -25,10 +27,12 @@ var Horizontal = {
 		this.startSize = {x:0,y:0};
 		this.loadImages = new Array();
 
+		this.current = $("container").getElement("strong.current");
+		this.max = $("container").getElement("strong.max");
 		this.searchValues = new Hash({
 			'api_key': this.apiKey, 'method': 'flickr.photos.search',
 			'sort': 'date-posted-desc', 'tags': this.tags.join(","),
-			'tag_mode': 'OR', 'per_page': 20, 'format': 'json'
+			'tag_mode': 'OR', 'per_page': this.perPage, 'format': 'json'
 		});
 /*
 		this.detailValues = new Hash({
@@ -38,9 +42,16 @@ var Horizontal = {
 */
 		this.flickr = new Request.JSONP({
 			'url': this.flickAPIURL, 'callbackKey': 'jsoncallback',
+			'onRequest': this.onRequest.bind(this),
 			'onSuccess': this.onSuccess.bind(this)
 		});
 		this.flickr.send({'data': this.searchValues});
+	},
+
+	onRequest: function() {
+	//	this.information = new Element("p", {"class": "loading"});
+	//	this.information.inject($("container"));
+	//	this.information.set("html", "now loading...");
 	},
 
 	onSuccess: function(response) {
@@ -70,24 +81,43 @@ var Horizontal = {
 		this.loadImages[index].inject(a);
 		a.inject(li);
 		li.inject(this.container);
+
+		var size = li.getSize();
+		var styleProps = {
+			"top": "50%",
+			"left": "50%",
+			"margin-top": -(size.y/2),
+			"margin-left": -(size.x/2)				
+		};
+		
+		li.setStyles(styleProps);
+		this.current.set("html", counter + 1);
+		this.max.set("html", index + 1);
 	},
 
 	onPreload: function() {
+		var elements = this.container.getElements("li");
+		elements.each(function(element,k){
+			element.setStyles({"top": 0, "left": 0, "margin-top": 0, "margin-left": 0});			
+		});
+
 		this.exhibition = new Exhibition.Horizontal(
-			this.container,
-			this.container.getElements("li"), {
+			this.container, elements, {
 				"onPreload": function(elements) {
 					var y1 = $("container").getSize().y;
 					var y2 = this.container.getSize().y;
-
 					this.container.setStyle("margin-top", (y1/2) - (y2/2));
+				}.bind(this),
+
+				"onActive": function(index, element) {
+					this.current.set("html", index + 1);
 				}.bind(this)
 			}
 		);
 		$("prev").addEvent("click", this.onPrevClick.bind(this));
 		$("next").addEvent("click", this.onNextClick.bind(this));
 
-		new Tips.Pointy(this.container.getElements("li a"), {pointyTipOptions: { point: 12, width: 200 }});
+		new Tips.Pointy(this.container.getElements("li a"), {pointyTipOptions: { point: 6, width: 200 }});
 	},
 
 	onNextClick: function() { this.exhibition.next(); },
