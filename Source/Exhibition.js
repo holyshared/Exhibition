fc87658ee31f71502117a8aff0fcae6cb15572cc
Noritaka Horio <holy.shared.design@gmail.com>
2010-02-08 01:31:55 +0900

    chnaged render logic.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Source/Exhibition.js
+++ Source/Exhibition.js
@@ -50,32 +50,176 @@ var Exhibition = new Class({
 		this.setEvents();
 	},
 
-	reset: function() { return true; },
+	reset: function() {
+		var positions = this.calculation();
+		positions.each(function(p,k){
+			var e = this.elements[k];
+			e.setStyle("left", p.x);
+			e.setStyle("top", p.y);
+		}, this);
+		this.elements.removeClass("active");
+		this.elements[this.index].addClass("active");
+	},
 
 	calculation: function() {
 		var size = this.container.getSize();
-		var x = size.x/2, y = size.y/2, l = size.x/2, t = size.y/2;
+		var x = size.x/2, y = size.y/2, left = size.x/2, top = size.y/2, height = 0;
+
+		this.positions = new Array();
+		this.elements.each(function(e,k) {
+			var size = e.getSize();
+			this.positions.push({x: left, y: top});
+			var index = k + 1;
+			if (index > 1 && (index % 5) == 0) {
+				left = x;
+				top = top + height + this.options.blank;
+			} else {
+				left = left + size.x + this.options.blank;
+			}
+			height = Math.max(height, size.y);
+		}, this);
+
+		var targets = [];
+		this.elements.each(function(e,k) {
+			var index = k + 1;
+			if (index > 1 && (index % 5) == 0) {
+				this.calculationHeight(targets);
+				targets = [];
+			}
+			targets.push(e);
+		}, this);
+
+
+
+
+
+
+
+		var e = this.elements[this.index];
+		var position = this.positions[this.index];
+		var ml = position.x - x + (e.getSize().x/2);
+		var mt = position.y - y + (e.getSize().y/2);
+		this.elements.each(function(e,k) {
+			this.positions[k].x = this.positions[k].x - ml;
+			this.positions[k].y = this.positions[k].y - mt;
+		}, this);
+
+		return this.positions;
+	},
+
+
+	calculationHeight: function(targets) {
+		var height = 0;
+		targets.each(function(e,k) { height = Math.max(e.getSize().y, height); })
+		targets.each(function(e,k) {
+			var index = this.elements.indexOf(e);
+			var position = this.positions[index];
+			var size = e.getSize();
+			var margin = (height - size.y) / 2;
+			this.positions[index].y = position.y + margin;
+		}, this);
+	},
+
+
+	calculationWidth: function(targets) {
+		var width = 0;
+		targets.each(function(e,k) { width = Math.max(e.getSize().x, width); })
+		targets.each(function(e,k) {
+			var index = this.elements.indexOf(e);
+			var position = this.positions[index];
+			var size = e.getSize();
+			var margin = (width - size.x) / 2;
+			this.positions[index].x = position.x + margin;
+		}, this);
+		return width;
+	},
+
+
+
+
+
+
+
+
+	adjustmentHeight: function(targets) {
+		var height = 0;
+		targets.each(function(e,k) {
+			height = Math.max(e.getSize().y, height);
+		})
+
+		targets.each(function(e,k) {
+			var size = e.getSize(), position = e.getPosition();
+			var margin = (height - size.y) / 2;
+			e.setStyle("top", position.y + margin);
+		})
+	},
+
+
+	adjustmentWidth: function(targets) {
+		var width = 0;
+		targets.each(function(e,k) {
+			width = Math.max(e.getSize().x, width);
+		})
+
+		targets.each(function(e,k) {
+			var size = e.getSize(), position = e.getPosition();
+			var margin = (width - size.x) / 2;
+			e.setStyle("left", position.x + margin);
+		})
+	},
+
+
+	adjustment: function() {
+		var targets = [];
+		this.elements.each(function(e,k) {
+			var index = k + 1;
+			if (index > 1 && (index % 5) == 0) {
+				this.adjustmentHeight(targets);
+				targets = [];
+			}
+			targets.push(e);
+		}, this);
+
+//1,6,11,17
+/*
+		var targets = [];
+		for (var i = 0; i < 5; i++) {
+			var c = i;
+			while(c < this.elements.length - 1){
+				targets.push(this.elements[c]);
+				c = c + 5;
+			}
+			this.adjustmentWidth(targets);
+
+		}
+	*/
+
+
+
+
+/*
 		var positions = new Array();
 		this.elements.each(function(e,k) {
 			var size = e.getSize();
-			positions.push({x: l, y: y});
-			if (k > 0 && (k % 5) == 0) {
-				l = x;
-
+			positions.push({x: left, y: top});
+			var index = k + 1;
+			if (index > 1 && (index % 5) == 0) {
+				left = x;
+				top = top + maxH + this.options.blank;
 			} else {
-				l = l + size.x + this.options.blank;
+				left = left + size.x + this.options.blank;
 			}
+			
+			maxH = Math.max(maxH, size.y);
+			
 		}, this);
+*/
 
-		var e = this.elements[this.index];
-		var m = positions[this.index].x - x + (e.getSize().x/2);
-		this.elements.each(function(e,k) {
-			positions[k].x = positions[k].x - m;
-		});
-		return positions;
+
+
+
+
 	},
-
-	adjustment: function() { return true; },
 
 	render:  function() {
 		var positions = this.calculation();
