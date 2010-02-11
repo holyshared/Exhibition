83206c752a2baaf87f954288918e4cc464181bd2
Noritaka Horio <holy.shared.design@gmail.com>
2010-02-11 22:49:57 +0900

    ie6/ie7/ie8 support.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Tests/js/vertical.js
+++ Tests/js/vertical.js
@@ -1,10 +1,12 @@
 window.addEvent("domready", function() {
+
+	var height = (Browser.Engine.trident && Browser.Engine.version <= 6) ? document.documentElement.clientHeight : window.innerHeight;
 	var container = $("container");
 	var exhibition = $("exhibition");
 
-	container.setStyle("height", window.innerHeight);
-	exhibition.setStyle("height", window.innerHeight);
-	
+	container.setStyle("height", height);
+	exhibition.setStyle("height", height);
+
 	var images = exhibition.getElements("li");
 	new Exhibition.Vertical(exhibition, images, {"defaultIndex": Math.round((images.length - 1)/2)});
 });
